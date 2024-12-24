5fa83e0563a7d9150ef992ac3183f70ad96c9d15
Shadab-2604 <138616478+Shadab-2604@users.noreply.github.com>
2024-12-24 14:14:59 +0530

    server.js

--- server.js
+++ server.js
@@ -30,6 +30,11 @@ app.use(passport.initialize());
 app.use(passport.session());
 app.use(flash());
 
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send('Something broke!');
+});
+
 // Passport config
 require('./config/passport')(passport);
 
@@ -48,4 +53,5 @@ app.use('/hisaab', require('./routes/hisaab'));
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, console.log(`Server running on port ${PORT}`));
+module.exports = app; // Add this line at the end of server.js
 
